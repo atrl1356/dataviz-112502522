4f07f156a673ca176d3dba9daa3913b97e3d5637
atrl1356 <148952474+atrl1356@users.noreply.github.com>
2023-11-12 23:46:02 +0800

    Add files via upload

---
+++ 112502522-dataviz/chart3/data3.js
@@ -0,0 +1,56 @@
+let population_109=[
+    {
+        "name": "印尼",
+        "count": 28093
+    },
+    {
+        "name":"菲律賓",
+        "count":33771
+    },
+    {
+        "name": "泰國",
+        "count": 16607
+    },
+    {
+        "name": "越南",
+        "count": 37733
+    }
+];
+
+let population_110=[
+    {
+        "name": "印尼",
+        "count": 25770
+    },
+    {
+        "name":"菲律賓",
+        "count":32881
+    },
+    {
+        "name": "泰國",
+        "count": 16877
+    },
+    {
+        "name": "越南",
+        "count": 37719
+    }
+];
+
+let population_111=[
+    {
+        "name": "印尼",
+        "count": 28107
+    },
+    {
+        "name":"菲律賓",
+        "count":36537
+    },
+    {
+        "name": "泰國",
+        "count": 20017
+    },
+    {
+        "name": "越南",
+        "count": 43853
+    }
+];
